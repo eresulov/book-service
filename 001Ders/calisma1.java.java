b66a599daccdaf74d65bb30e2c00c74e7732a0b6
eresulov <97796011+eresulov@users.noreply.github.com>
2022-01-18 18:23:21 +0400

    for inspection

---
+++ 001Ders/calisma1.java.java
@@ -0,0 +1,16 @@
+
+public class calisma1 {
+
+    public static void main(String[] args) {
+        int cem = 0;
+        for (int i = 5; i <= 55; i++) {
+
+            if (i % 5 == 0) {
+                cem += i;
+            }
+        }
+        System.out.println("cem=" + cem);
+
+    }
+
+}
---
+++ 001Ders/calisma2.java.java
@@ -0,0 +1,12 @@
+
+public class Calisma2 {
+
+    public static void main(String[] args) {
+        for (int i = 30; i <= 70; i++) {
+
+            if (i % 7 == 0) {
+                System.out.println("reqem=" + i);
+            }
+        }
+    }
+}
