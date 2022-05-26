d5521fc7a6f7d18452ac8eae6db920184b1d682d
eresulov <97796011+eresulov@users.noreply.github.com>
2022-05-27 02:41:52 +0400

    29 Update

--- java-spring/student-crud/Studentfront/script.js
+++ java-spring/student-crud/Studentfront/script.js
@@ -1,9 +1,9 @@
 var selectedStudentId = 0;
 
 var API_URL = "http://localhost:8585"; //secilmis telebenin id sini bu deyisene memimsedirik
-var username=localStorage.getItem('username');
-var password=localStorage.getItem('password');
-var token="Basic "+window.btoa(username+":"+password);
+var username = localStorage.getItem('username');
+var password = localStorage.getItem('password');
+var token = "Basic " + window.btoa(username + ":" + password);
 
 var gridOptionsGlobal;
 
@@ -21,25 +21,25 @@ var surnameErrorElement = document.getElementById('surname-error');
 
 var studentNoteInput = document.getElementById('student-note');
 
- async function onSaveStudent(event) {
+async function onSaveStudent(event) {
 
     event.preventDefault(); //form un default gelen ozelliyini legv edirik form yenilemeli deyil mane olur 
-    
-    let formData=new FormData();
-    let photo=studentProfilePhotoInput.files[0];
 
-    formData.append("file",photo);
-    let response=await fetch(API_URL+'/files/upload',{
-method:"POST",
-body:formData,
-headers:{
-    "Authorization":token
-}
+    let formData = new FormData();
+    let photo = studentProfilePhotoInput.files[0];
+
+    formData.append("file", photo);
+    let response = await fetch(API_URL + '/files/upload', {
+        method: "POST",
+        body: formData,
+        headers: {
+            "Authorization": token
+        }
     });
-   
-    var photoName=await response.json(); //server nevaxt cavab verir onda ise dusur 
 
-saveStudent(photoName.fileName);
+    var photoName = await response.json(); //server nevaxt cavab verir onda ise dusur 
+
+    saveStudent(photoName.fileName);
 
 }
 
@@ -52,38 +52,39 @@ function loadAllStudents() {
     }
 
     http.open("GET", API_URL + "/students", true)
-    http.setRequestHeader("Authorization",token); //authorization header 
+    http.setRequestHeader("Authorization", token); //authorization header 
     http.send();
 }
 
 function fillStudentsTable(students) {
-   gridOptionsGlobal.api.setRowData(students); //bos olan mekumati doldurur row un ici
+    gridOptionsGlobal.api.setRowData(students); //bos olan mekumati doldurur row un ici
 }
 
 
 function onDeleteStudent() {
-  var selectedStudents=gridOptionsGlobal.api.getSelectedRows();
-    
-  var selectedStudents=gridOptionsGlobal.api.getSelectedRows();
-  if(length>0){
+    var selectedStudents = gridOptionsGlobal.api.getSelectedRows();
+    if (selectedStudents.length > 0) {
 
-  
-    if (confirm('Silmeye eminsen?')) {
-var studentId = [0].id; //siyahidan secilen telebelerin obyektini qaytarir Massiv
-        var http = new XMLHttpRequest();
-        http.onload = function () {
-            loadAllStudents(); // birce defe sehifeni yenilemek ucundur 
+
+        if (confirm('Silmeye eminsen?')) {
+            var studentId = selectedStudents[0].id; //siyahidan secilen telebelerin obyektini qaytarir Massiv
+            var http = new XMLHttpRequest();
+            http.onload = function () {
+                loadAllStudents(); // birce defe sehifeni yenilemek ucundur 
+            }
+
+            http.open("DELETE", API_URL + "/students/" + studentId, true)
+            http.setRequestHeader("Authorization", token);
+            http.send();
         }
-
-        http.open("DELETE", API_URL + "/students/" + studentId, true)
-        http.setRequestHeader("Authorization",token);
-        http.send();
-    }
-} else 
-alert('Siyahidan silmek istediyinizi secin!');
+    } else
+        alert('Siyahidan silmek istediyinizi secin!');
 }
 
-function onEditStudent(studentId) {
+function onEditStudent() {
+    var selectedStudents = gridOptionsGlobal.api.getSelectedRows();
+    if (selectedStudents.length==1) {
+        var studentId=selectedStudents[0].id;
     selectedStudentId = studentId;
     setHeaderText('Telebe Redaqtesi id si -> ' + studentId);
     var http = new XMLHttpRequest();
@@ -95,9 +96,9 @@ function onEditStudent(studentId) {
     }
 
     http.open("GET", API_URL + "/students/" + studentId, true) // id sine gore melumati qaytaracaq
-    http.setRequestHeader("Authorization",token);
+    http.setRequestHeader("Authorization", token);
     http.send();
-    
+    }
 }
 
 function setHeaderText(text) {
@@ -141,7 +142,7 @@ function onSaveStudentNote(event) {
 
     http.open("POST", API_URL + "/student-notes", true) //metodun tipi , cagiracagimiz yer 
     http.setRequestHeader("Content-Type", "application/json")
-    http.setRequestHeader("Authorization",token);
+    http.setRequestHeader("Authorization", token);
 
     http.send(JSON.stringify(studentNoteObject)); // json un stringfy funksiyasi json a cevirir 
 
@@ -172,39 +173,52 @@ function loadAllStudentNotes(studentId) {
     }
 
     http.open("GET", API_URL + "/student-notes/student/" + studentId, true)
-    http.setRequestHeader("Authorization",token);
+    http.setRequestHeader("Authorization", token);
     http.send();
 }
-function prepareAgGridTable(){
+
+function prepareAgGridTable() {
     const columnDefs = [ //sutunlar teyin edilir
-        { field: "id" ,headerName :"Kod"},
-        { field: "name",headerName :"Ad" },
-        { field: "surname" ,headerName :"Soyad"}
-      ];
-      
-   
-      const gridOptions = {
+        {
+            field: "id",
+            headerName: "Kod"
+        },
+        {
+            field: "name",
+            headerName: "Ad"
+        },
+        {
+            field: "surname",
+            headerName: "Soyad"
+        }
+    ];
+
+
+    const gridOptions = {
         columnDefs: columnDefs,
         rowData: [],
-        defaultColDef:{sortable:true,filter : true}, //sortlasdirir 
-        animateRows:true, //animasiyali sort
-        floatingFilter:true,
-        pagination:true, //hisse hisse verir page lere bolur
-        rowSelection:'multiple'
-      }; //ayarlari sazlanir ag-grid in 
-      
-      gridOptionsGlobal=gridOptions; //grid options bu funksiyada local oldugu ucun globalda grid option deyiseni yaratmaliyiq
+        defaultColDef: {
+            sortable: true,
+            filter: true
+        }, //sortlasdirir 
+        animateRows: true, //animasiyali sort
+        floatingFilter: true,
+        pagination: true, //hisse hisse verir page lere bolur
+        rowSelection: 'multiple'
+    }; //ayarlari sazlanir ag-grid in 
 
-      //serverden html sehifesi yuklenende bu kod calisir myStudents e ag grid sazlanir
-      document.addEventListener('DOMContentLoaded', () => {
-          const gridDiv = document.querySelector('#myStudents');
-          new agGrid.Grid(gridDiv, gridOptions);
-      });
+    gridOptionsGlobal = gridOptions; //grid options bu funksiyada local oldugu ucun globalda grid option deyiseni yaratmaliyiq
+
+    //serverden html sehifesi yuklenende bu kod calisir myStudents e ag grid sazlanir
+    document.addEventListener('DOMContentLoaded', () => {
+        const gridDiv = document.querySelector('#myStudents');
+        new agGrid.Grid(gridDiv, gridOptions);
+    });
 }
 prepareAgGridTable();
 loadAllStudents();
 
-function saveStudent(photo){
+function saveStudent(photo) {
     var studentName = studentNameInput.value // adini yazacagimiz inputu qaytarir
     var studentSurname = studentSurnameInput.value;
 
@@ -212,7 +226,7 @@ function saveStudent(photo){
     studentObject.id = selectedStudentId; //secdiyimiz id update edir yeni telebe etmir daha 
     studentObject.name = studentName; // (name ) back end le ad eyni olmalidir
     studentObject.surname = studentSurname;
-    studentObject.profilePhoto=photo;
+    studentObject.profilePhoto = photo;
     var http = new XMLHttpRequest(); //ajax request sinifinnen obyekt yaradiriq
 
     http.onload = function () {
@@ -234,22 +248,23 @@ function saveStudent(photo){
             nameErrorElement.innerHTML = nameError; //id si name-error dursa
             surnameErrorElement.innerHTML = surnameError;
         } else {
-            clearErrorMessages();gridOptionsGlobal.api.getSelectedRows();
-                        selectedStudentId = 0; // redakte etdikden sonra yeniden 0 edirikki telebe qeydiyatina kecsin
+            clearErrorMessages();
+            gridOptionsGlobal.api.getSelectedRows();
+            selectedStudentId = 0; // redakte etdikden sonra yeniden 0 edirikki telebe qeydiyatina kecsin
             setHeaderText('Yeni Telebe Qeydiyyati ');
             loadAllStudents();
         }
     }
     http.open("POST", API_URL + "/students", true) //cagiracagimiz yer 
     http.setRequestHeader("Content-Type", "application/json")
-    http.setRequestHeader("Authorization",token);
+    http.setRequestHeader("Authorization", token);
     http.send(JSON.stringify(studentObject)); // json un stringfy funksiyasi json a cevirir 
 }
 
 
-function onShowImage(){
-var selectedStudents=gridOptionsGlobal.api.getSelectedRows(); //ag-gridden secilen setrleri obyekt sekilde qaytarir
-console.log(selectedStudents[0]); 
+function onShowImage() {
+    var selectedStudents = gridOptionsGlobal.api.getSelectedRows(); //ag-gridden secilen setrleri obyekt sekilde qaytarir
+    console.log(selectedStudents[0]);
 
-document.getElementById('student-image').src=   API_URL +"/files/download/"+ selectedStudents[0].profilePhoto;
+    document.getElementById('student-image').src = API_URL + "/files/download/" + selectedStudents[0].profilePhoto;
 }
