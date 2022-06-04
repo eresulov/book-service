a99fb6542e99d87ed15f2a7ef3fa7b39161c2bf6
eresulov <97796011+eresulov@users.noreply.github.com>
2022-06-04 18:49:01 +0400

    movzu
    movzu

--- java-spring/student-crud/student-crud/src/main/java/az/developia/studentcrud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package az.developia.studentcrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import az.developia.studentcrud.model.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, String>{
-
-}
